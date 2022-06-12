cec3aeaeefb8417a21f63d6497e99c429eedb303
James <jame31560@gmail.com>
2022-06-12 17:16:09 +0800

    docs(comment): Add comment
    None
    
    Related issue: None

--- model/employee.py
+++ model/employee.py
@@ -9,14 +9,19 @@ class Employee():
         self.employee = employee
 
     def addEmployee(self, employee):
+        # Append a employee who are self management to employee list.
         self.employee.append(employee)
 
     def toStr(self, level=0):
         result = ""
+        # level will generate different indent in front of result
         result += "{}{}\n".format("    "*level, self.firstName)
+        # if don't have employee under self then return 
         if len(self.employee) <= 0:
             return result
+        # print next level employees
         result += "{}Employees of: {}\n".format("    "*level, self.firstName)
         for employee in self.employee:
+            # level add 1
             result += employee.toStr(level + 1)
         return result
